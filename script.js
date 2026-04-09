73c9ee97b8335321a94adf9a7b51868869f7fb25
Matheus <matheus.victor.ss48@gmail.com>
2026-04-09 08:52:06 -0300

    imprimindo temperatura na tela

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
 fetch("https://api.open-meteo.com/v1/forecast?latitude=-23.1864&longitude=-46.8842&hourly=temperature_2m")
 .then(res=>res.json())
 .then(dados =>{
     console.log(dados)
+    document.getElementById("resultado").innerHTML =
+  dados.hourly.temperature_2m[0] + "°C";
 })
