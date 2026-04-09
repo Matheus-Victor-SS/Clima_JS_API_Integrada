762fb1e590748c82564c1a841ddf81b01f113aa7
Matheus <matheus.victor.ss48@gmail.com>
2026-04-09 09:10:44 -0300

    adicionando horário atual corrigido com 0 antes de números menores que 9

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
+function atualizar(){
+var res=document.getElementById('horario')
+var data = new Date()//importando Date()
+        var hora = data.getHours()<=9 ? "0"+data.getHours(): data.getHours()
+        var minuto = data.getMinutes()<=9 ? "0"+data.getMinutes(): data.getMinutes(),
+         segundos = data.getSeconds() <=9 ? "0"+data.getSeconds(): data.getSeconds()
+         horario.innerHTML = `${hora}:${minuto}:${segundos}`//coloca um do lado do outro
+}
+setInterval(Atualizar, 1000)
 
+//entra em contato com a API
 fetch("https://api.open-meteo.com/v1/forecast?latitude=-23.1864&longitude=-46.8842&hourly=temperature_2m")
+//tranforma a resposta dela em json
 .then(res=>res.json())
 .then(dados =>{
     console.log(dados)
-    document.getElementById("resultado").innerHTML =
+    //imprimindo temperatura
+    document.getElementById("temp").innerHTML =
   dados.hourly.temperature_2m[0] + "°C";
+  document.getElementById("umidade").innerHTML =
+  dados.hourly.umidade_relativa_2m + " %";
+  document.getElementById("vento").innerHTML =
+  dados.hourly.velocidade_do_vento_10m + " km/h";
 })
+
