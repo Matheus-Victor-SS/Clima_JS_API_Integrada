d62bf570525f7443e2b577d5893660f0762cfa5c
Matheus <matheus.victor.ss48@gmail.com>
2026-04-09 10:11:24 -0300

    valor da chuva tambem e mudanças no html

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(
     var long = (city.results[0].longitude)
     }
 //entra em contato com a API e coloca os valores de latitude e longitude convertidos
-fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`)
+fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,rain`)
 //tranforma a resposta dela em json
 .then(res=>res.json())
 .then(dados =>{
@@ -45,6 +45,8 @@ fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&
   dados.hourly.relative_humidity_2m[0] + " %";
   document.getElementById("vento").innerHTML =
   dados.hourly.wind_speed_10m[0] + " km/h";
+    document.getElementById("chuva").innerHTML =
+  dados.hourly.rain[0] + " mm";
   })
 })
 }
