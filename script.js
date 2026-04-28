fac03a98a101508fb3dba2e6f8165b46d68cdf92
Matheus <matheus.victor.ss48@gmail.com>
2026-04-28 11:18:42 -0300

    mudando tom dos fundos e colocando o parcial nublado que esqueci no js

--- script.js
+++ script.js
@@ -106,7 +106,7 @@ fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&
     //pega o numero traduzido e substitui no local do HTML
     var weathercode = dados.hourly.weathercode[0];
      document.getElementById("climaDescricao").innerHTML = traduzirClima(weathercode);
-mudarCorFundo(weathercode);
+    mudarCorFundo(weathercode);
 
     //imprimindo valores
     document.getElementById("temp").innerHTML =
@@ -142,7 +142,7 @@ function mudarCorFundo(weathercode) {
     const body = document.body;
     
     // Remove todas as classes de clima anteriores
-    body.classList.remove('clima-limpo', 'clima-nublado', 'clima-chuva', 'clima-neve', 'clima-tempestade');
+    body.classList.remove('clima-limpo', 'clima-parcial', 'clima-nublado', 'clima-chuva', 'clima-neve', 'clima-tempestade');
     
     // Adiciona a classe conforme o clima(muda o CSS)
     if (weathercode === 0) {
